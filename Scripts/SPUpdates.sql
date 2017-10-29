5ac756fc8538005f0c643016cc0838f46c27aee7
Elmanuchuky <elmanu.chuky@gmail.com>
2017-10-29 11:01:10 -0300

    1

--- Scripts/SPUpdates.sql
+++ Scripts/SPUpdates.sql
@@ -1,1 +1,11 @@
+GO
 -- SP Update de Alquileres
+CREATE PROCEDURE sp_update_alquiler
+@mCantidadComputadoras int,
+@mCantidadSillasAdicionales int,
+@mTieneAccesoSalaReuniones bit
+AS
+BEGIN
+	UPDATE Alquileres
+	SET cantidad_computadoras = @mCantidadComputadoras, cantidad_sillas_adicionales = @mCantidadSillasAdicionales, tine_acceso_sala_reuniones = @mTieneAccesoSalaReuniones
+END
